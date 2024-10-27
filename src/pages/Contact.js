e2905920853473fb4cd381754eb01933ae507908
olaniyihope2 <olaniyihope2@gmail.com>
2024-10-27 22:01:44 +0100

    all

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -36,10 +36,10 @@ const Contact = () => {
                           property="item"
                           typeof="WebPage"
                           title="Go to Suxnix."
-                          href="https://themegenix.net/wp/suxnix"
+                          href="/"
                           class="home"
                         >
-                          <span property="name">Suxnix</span>
+                          <span property="name">Home</span>
                         </a>
                       </span>
                       <span class="breadcrumb-separator">
@@ -109,15 +109,14 @@ const Contact = () => {
 
                                       <i
                                         aria-hidden="true"
-                                        class="far fa-map"
+                                        class="fab fa-instagram"
                                       ></i>
                                     </div>
                                     <div class="contact-content">
-                                      <h5 class="title">Office Address</h5>
+                                      <h5 class="title">Social Media</h5>
 
                                       <p class="contact-desc">
-                                        99 NY Address Street, Brooklyn, United
-                                        State
+                                        @divinehealthcare
                                       </p>
                                     </div>
                                   </div>
@@ -138,7 +137,7 @@ const Contact = () => {
                                       <h5 class="title">Phone Number</h5>
 
                                       <p class="contact-desc">
-                                        875 7556 464 7658 <br /> 765 648 567 98
+                                        +234 811 320 1322
                                       </p>
                                     </div>
                                   </div>
@@ -159,8 +158,7 @@ const Contact = () => {
                                       <h5 class="title">Web Connect</h5>
 
                                       <p class="contact-desc">
-                                        info@suxnixmail.com <br />{" "}
-                                        suxnixexample.com
+                                        info@divinehealthcare.com <br />{" "}
                                       </p>
                                     </div>
                                   </div>
@@ -173,12 +171,7 @@ const Contact = () => {
                         <section class="contact-form-area">
                           <div class="container">
                             <div class="row">
-                              <div class="col-xl-6">
-                                <div id="contact-map">
-                                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d96811.54759587669!2d-74.01263924803828!3d40.6880494567041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25bae694479a3%3A0xb9949385da52e69e!2sBarclays%20Center!5e0!3m2!1sen!2sbd!4v1636195194646!5m2!1sen!2sbd"></iframe>{" "}
-                                </div>
-                              </div>
-                              <div class="col-xl-6">
+                              <div class="col-xl-12">
                                 <div class="contact-form-wrap">
                                   <div class="section-title mb-50">
                                     <p class="sub-title">.. request make ..</p>
@@ -357,103 +350,6 @@ const Contact = () => {
                             </div>
                           </div>
                         </section>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </section>
-            <section
-              class="elementor-section elementor-top-section elementor-element elementor-element-9b4e891 elementor-section-full_width elementor-section-height-default elementor-section-height-default"
-              data-id="9b4e891"
-              data-element_type="section"
-            >
-              <div class="elementor-container elementor-column-gap-no">
-                <div
-                  class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-ab2673f"
-                  data-id="ab2673f"
-                  data-element_type="column"
-                >
-                  <div class="elementor-widget-wrap elementor-element-populated">
-                    <div
-                      class="elementor-element elementor-element-690dc3a elementor-widget elementor-widget-tg-gallery"
-                      data-id="690dc3a"
-                      data-element_type="widget"
-                      data-widget_type="tg-gallery.default"
-                    >
-                      <div class="elementor-widget-container">
-                        <div class="footer-instagram">
-                          <div class="container">
-                            <div class="row g-0 instagram-active">
-                              <div class="col-2">
-                                <div class="footer-insta-item">
-                                  <a href="../wp-content/uploads/2022/09/instagram_post01.jpg">
-                                    <img
-                                      decoding="async"
-                                      src="../wp-content/uploads/2022/09/instagram_post01.jpg"
-                                      alt=""
-                                    />
-                                  </a>
-                                </div>
-                              </div>
-                              <div class="col-2">
-                                <div class="footer-insta-item">
-                                  <a href="../wp-content/uploads/2022/09/instagram_post02.jpg">
-                                    <img
-                                      decoding="async"
-                                      src="../wp-content/uploads/2022/09/instagram_post02.jpg"
-                                      alt=""
-                                    />
-                                  </a>
-                                </div>
-                              </div>
-                              <div class="col-2">
-                                <div class="footer-insta-item">
-                                  <a href="../wp-content/uploads/2022/09/instagram_post03.jpg">
-                                    <img
-                                      decoding="async"
-                                      src="../wp-content/uploads/2022/09/instagram_post03.jpg"
-                                      alt=""
-                                    />
-                                  </a>
-                                </div>
-                              </div>
-                              <div class="col-2">
-                                <div class="footer-insta-item">
-                                  <a href="../wp-content/uploads/2022/09/instagram_post04.jpg">
-                                    <img
-                                      decoding="async"
-                                      src="../wp-content/uploads/2022/09/instagram_post04.jpg"
-                                      alt=""
-                                    />
-                                  </a>
-                                </div>
-                              </div>
-                              <div class="col-2">
-                                <div class="footer-insta-item">
-                                  <a href="../wp-content/uploads/2022/09/instagram_post05.jpg">
-                                    <img
-                                      decoding="async"
-                                      src="../wp-content/uploads/2022/09/instagram_post05.jpg"
-                                      alt=""
-                                    />
-                                  </a>
-                                </div>
-                              </div>
-                              <div class="col-2">
-                                <div class="footer-insta-item">
-                                  <a href="../wp-content/uploads/2022/09/instagram_post06.jpg">
-                                    <img
-                                      decoding="async"
-                                      src="../wp-content/uploads/2022/09/instagram_post06.jpg"
-                                      alt=""
-                                    />
-                                  </a>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
                       </div>
                     </div>
                   </div>
